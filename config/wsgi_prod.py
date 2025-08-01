26977226d7ac9744f5dbb362e814cc340d006de7
Anna <anna.yim@spintech.co.kr>
2025-08-01 10:17:15 +0900

    8단계: 배포 환경 구성 (운영용 WSGI, requirements.txt, .gitignore 추가)

---
+++ config/wsgi_prod.py
@@ -0,0 +1,6 @@
+# 운영 환경용 WSGI 설정 예시
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+application = get_wsgi_application()
