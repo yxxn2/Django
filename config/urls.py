e4e9a482bb282d0fcf6b2249246b75321ce2cd07
Anna <anna.yim@spintech.co.kr>
2025-08-01 11:07:07 +0900

    루트 경로에 메인 페이지 연결

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from core.frontend_views import home
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
 ]
